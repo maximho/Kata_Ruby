417e5a836e254d1f6b583890057e1a292f91fa52
Maximilianoho <maximho20@gmail.com>
2015-09-18 19:27:49 +0200

    miles per gallon to kilometers per liter

---
+++ milles:gallon_kilometers:liter_kata_8.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "pry"
+
+def converter(mpg)
+  ("%.2f" % ((mpg * 1.609344) / 4.54609188)).to_f
+  
+
+end
+
+class Mytest < Minitest::Test
+
+	def test_things
+		assert_equal(converter(12), 4.25)
+		assert_equal(converter(24), 8.50)
+		assert_equal(converter(36), 12.74)
+	end
+end
